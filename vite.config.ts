99b1efadd5ac9380a09cdb77bcb138ca50da9862
Keyur Gohil <133516013+KeyurGohilUK@users.noreply.github.com>
2026-09-20 13:19:50 +0100

    Build public and admin pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,14 @@
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/restaurant-web-app/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin.html'),
+      },
+    },
+  },
 });
